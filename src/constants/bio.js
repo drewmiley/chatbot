7a92169f1b4f2e63a37386c471c6f3a1c9e64323
Drew Miley <dmiley@scottlogic.co.uk>
2016-08-29 16:43:58 +0100

    Adds bio statements

--- src/constants/bio.js
+++ src/constants/bio.js
@@ -1,5 +1,12 @@
 import {List} from 'immutable';
 
-const bio = List.of('to be determined');
+const bio = List.of('endless sentimentality',
+    'beastly unit tester',
+    'in need of a naked lunch',
+    'pool aficionado',
+    'pun-meister',
+    'pointless anecdote regaler',
+    'recovering mathematician',
+    'requires a welcome planet');
 
 export default bio;
