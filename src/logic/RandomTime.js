2397d812465768b75749de057e19b760a0bdce25
Drew Miley <dmiley@scottlogic.co.uk>
2016-09-01 14:54:30 +0100

    Deploy to gh-pages

--- src/logic/RandomTime.js
+++
@@ -1,7 +0,0 @@
-export default function RandomTime() {
-    const seed = (s) => {
-        s = Math.sin(s) * 10000;
-        return s - Math.floor(s);
-    };
-    return 1000 + Math.floor(4000 * seed(Math.random()));
-}
